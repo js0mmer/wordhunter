d69210ae3b2d546aaa9286be0a20c0072de0fd9f
Darren Chui <Chui62098@PLTW.local>
2019-09-17 10:32:35 -0700

    Removed unnecessary comments and merge with origin base

--- StartScreen.java
+++ StartScreen.java
@@ -1,62 +1,4 @@
-<<<<<<< HEAD
-/*    */ import greenfoot.World;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class StartScreen
-/*    */   extends World
-/*    */ {
-/*    */   public StartScreen() {
-/* 19 */     super(1200, 800, 1);
-/* 20 */     prepare();
-/*    */   }
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   private void prepare() {
-/* 29 */     INSTRUCTIONS iNSTRUCTIONS = new INSTRUCTIONS();
-/* 30 */     addObject(iNSTRUCTIONS, 593, 596);
-/* 31 */     BEGIN sTART = new BEGIN();
-/* 32 */     addObject(sTART, 600, 476);
-/* 33 */     QUIT qUIT = new QUIT();
-/* 34 */     addObject(qUIT, 593, 704);
-             TitleMenu title = new TitleMenu();
-             addObject(title, 200, 200);
-/*    */   }
-/*    */ }
-
-
-/* Location:              C:\Users\jacob\Desktop\jd\greenfoot6984158792478336299.jar!\StartScreen.class
- * Java compiler version: 11 (55.0)
- * JD-Core Version:       1.0.7
- */
-=======
 import greenfoot.World;
-
-
-
-
-
-
-
-
-
-
-
-
 
 public class StartScreen
   extends World
@@ -65,11 +7,6 @@ public class StartScreen
     super(1200, 800, 1);
     prepare();
   }
-
-
-
-
-
   
   private void prepare() {
     INSTRUCTIONS iNSTRUCTIONS = new INSTRUCTIONS();
@@ -78,8 +15,10 @@ public class StartScreen
     addObject(sTART, 600, 476);
     QUIT qUIT = new QUIT();
     addObject(qUIT, 593, 704);
+    TitleMenu title = new TitleMenu();
+    addObject(title, 200, 200);
   }
 }
 
 
->>>>>>> abeccdadea413c761b053ae09cfe2a4b84545234
+
